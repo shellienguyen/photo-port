67a9f62bf3b4556dac3a813bd0eeeb3bd4491911
sidonie <shelliednnguyen@gmail.com>
2021-04-26 10:12:17 -0700

    About section and nav bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+/*
+This App.js file is the center of the application. Think of App.js as the
+root component, or the wrapper component that houses all of the other
+components. To effect any change on the application, we need to either
+modify this file or add components inside it.
+
+Need to import React in every component file
+*/
+import React from 'react';
 import './App.css';
+import About from './components/About';
+import Nav from './components/Nav';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   return (
+      <div>
+         <Nav></Nav>
+         <main>
+            <About></About>
+         </main>
+      </div>
+   );
+};
+
 
 export default App;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Nav = () => {
+   const categories = [
+      { name: "commercial", description: "Photos of grocery stores, food trucks, and other commercial projects" },
+      { name: "portraits", description: "Portraits of people in my life" },
+      { name: "food", description: "Delicious delicacies" },
+      { name: "landscape", description: "Fields, farmhouses, waterfalls, and the beauty of nature" },
+   ];
+
+   function categorySelected( name ) {
+      console.log( `${name} clicked` );
+   };
+
+   return (
+      <header>
+         <h2>
+            <a href="/"><span role="img" aria-label="camera"> 📸</span> Oh Snap!</a>
+         </h2>
+
+         <nav>
+            <ul className="flex-row">
+               <li className="mx-2"><a href="#about">About me</a></li>
+               <li><span>Contact</span></li>
+               {/*
+               Whenever we map over anything in JSX, the outermost element
+               must have a key attribute that's set to be something unique.
+               This helps React keep track of items in the virtual DOM.
+               When mapping over an array in a JSX expression, return only a
+               single JSX element.
+               */}
+               {categories.map(( category ) => (
+                  <li className="mx-1" key={category.name}>
+                     {/*
+                     The onClick() attribute is expecting a callback function
+                     declaration. It's important to wrap it in a function
+                     declaration rather than just calling
+                     categorySelected(category.name), which would cause the
+                     function to get called whenever the component renders as well.
+                     */}
+                     <span onClick={() => categorySelected( category.name )}>{category.name}</span>
+                  </li>
+               ))}
+            </ul>
+         </nav>
+      </header>
+   );
+};
+
+
+export default Nav;
