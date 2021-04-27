cbd7a36a264d1adf8a0fa14cf632ad176136ba72
sidonie <shelliednnguyen@gmail.com>
2021-04-27 12:52:11 -0700

    added contact form fields

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 
 function App() {
